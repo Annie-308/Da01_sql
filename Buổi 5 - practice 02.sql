59e5b3fa58882d8342a6419d94369410bc0cec23
Annie-308 <lehongphuc300804@gmail.com>
2024-07-01 17:39:03 +0700

    Update Buổi 5 - practice 02.sql

--- Buổi 5 - practice 02.sql
+++ Buổi 5 - practice 02.sql
@@ -1,1 +1,45 @@
+-- EX 01
+Select distinct city from station
+where id%2=0
+-- EX 02
+Select count(city) - count(distinct city) from station
+-- EX 04
+SELECT round(cast(sum(order_occurrences*item_count)/sum(order_occurrences)
+as decimal),1)
+from items_per_order
+-- EX 05
+SELECT candidate_id from candidates
+where skill in ('Python', 'Tableau', 'PostgreSQL')
+group by candidate_id
+having count(skill)=3;
+-- EX 06
+SELECT user_id,
+date(max(post_date)) - date(min(post_date)) as days_between
+from posts
+where post_date between '01-01-2021' and '01-01-2022'
+group by user_id
+having date(max(post_date)) - date(min(post_date)) > 0
+-- EX 07
+SELECT card_name,
+max(issued_amount)-min(issued_amount) as difference
+FROM monthly_cards_issued
+group by card_name 
+order by max(issued_amount)-min(issued_amount) DESC
+-- EX 08
+SELECT manufacturer,
+count(drug) as drug_count, 
+sum(cogs-total_sales) as total_loss
+from pharmacy_sales
+where total_sales<cogs
+group by manufacturer
+order by total_loss DESC
+-- EX 09
+select id, movie, description, rating
+from cinema
+where description <> 'boring'
+having id%2 <> 0
+order by rating desc
+-- EX 10
 
+-- EX 11
+-- EX 12
