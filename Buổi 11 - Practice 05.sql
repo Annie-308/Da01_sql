fc6004c5ecf92cc943be1d988f1881646f62fc4d
Annie-308 <lehongphuc300804@gmail.com>
2024-07-07 23:56:17 +0700

    Update Buổi 11 - Practice 05.sql

--- Buổi 11 - Practice 05.sql
+++ Buổi 11 - Practice 05.sql
@@ -4,6 +4,7 @@ FROM COUNTRY AS a
 INNER JOIN CITY as b
 on a.code=b.countrycode
 group by  a.continent
+  
 -- EX 02
 select 
 round(cast(count(b.email_id) as decimal)/count(distinct a.email_id),2) as activation_rate
@@ -11,6 +12,7 @@ from emails as a
 left join texts as b  
 on a.email_id=b.email_id
 and b.signup_action = 'Confirmed'
+  
 -- EX 03
 select b.age_bucket,
 round(sum(a.time_spent) FILTER (WHERE a.activity_type = 'send')*100.00/sum(a.time_spent),2) as send_perc,
@@ -20,6 +22,7 @@ inner join age_breakdown as b
 on a.user_id=b.user_id
 where a.activity_type in ('open','send')
 group by b.age_bucket
+  
 -- EX 04
 SELECT customer_id
 FROM customer_contracts as a
@@ -27,6 +30,7 @@ left join products as b
 on a.product_id=b.product_id
 group by customer_id
 having count(DISTINCT product_category) = 3
+  
 -- EX 05
 select m.employee_id, m.name, count(e.employee_id) as reports_count,
 round(avg(e.age),0) as average_age
@@ -34,6 +38,7 @@ from Employees as e
 join Employees as m
 on e.reports_to=m.employee_id
 group by employee_id
+  
 -- EX 06
 select P.product_name, SUM(O.unit) as unit
 from Products as P
@@ -42,6 +47,7 @@ on P.product_id=O.product_id
 where O.order_date between '2020-02-01' and '2020-02-28'
 group by O.product_id
 having SUM(O.unit)>=100 
+  
 -- EX 07
 SELECT a.page_id
 from pages as a  
