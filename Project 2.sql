f7afed056199335c95c0b53d818712720033afcd
Annie-308 <lehongphuc300804@gmail.com>
2024-07-28 13:30:19 +0700

    Update Project 2.sql

--- Project 2.sql
+++ Project 2.sql
@@ -1,21 +1,23 @@
--- Bài 1: order by chưa đúng
-select extract(year from created_at) || '-' || extract(month from created_at) as year_month,
+-- Bài 1: 
+select  FORMAT_DATE('%Y-%m', created_at) as year_month,
     count(distinct order_id) as total_order, 
     count(distinct user_id) as total_user
 from bigquery-public-data.thelook_ecommerce.orders
-where status='Shipped' and extract(year from created_at) || '-' || extract(month from created_at) between '2019-01' and '2022-04'
-group by extract(year from created_at) || '-' || extract(month from created_at)
+where status='Shipped' and FORMAT_DATE('%Y-%m', created_at) between '2019-01' and '2022-04'
+group by FORMAT_DATE('%Y-%m', created_at)
 order by year_month
-
--- Bài 2: order by chưa đúng
-select extract(year from a.created_at) || '-' || extract(month from a.created_at) as year_month,
+/*
+*/
+-- Bài 2: 
+select FORMAT_DATE('%Y-%m', a.created_at) as year_month,
     count(distinct a.user_id) as distinct_user,
     round(sum(b.sale_price)/count(a.order_id),2) as AOV
 from bigquery-public-data.thelook_ecommerce.orders as a join bigquery-public-data.thelook_ecommerce.order_items as b on a.order_id=b.order_id
-where extract(year from a.created_at) || '-' || extract(month from a.created_at) between '2019-01' and '2022-04'
+where FORMAT_DATE('%Y-%m',a.created_at) between '2019-01' and '2022-04'
 group by year_month
 order by year_month
-
+/*
+*/
 --Bài 3
 with cte as(
     select  first_name, last_name, age, gender, 'yongest' as tag
